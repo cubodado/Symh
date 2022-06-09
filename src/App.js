3f7e516b9cb8f390dcdd3a434e213aa7b2771496
cubodado <dalkang000@gmail.com>
2022-06-09 19:00:28 +0900

    Create: LandingText component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import LandingText from './LandingPage/LandingText';
 
 function App() {
-  return <div className="wrapper"></div>;
+  return (
+    <div className="wrapper">
+      <LandingText />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/LandingPage/LandingText.js
@@ -0,0 +1,11 @@
+import './LandingText.css';
+
+const LandingText = () => {
+  return (
+    <div>
+      <p id="landing-text">Send your moving heart 💖</p>;
+    </div>
+  );
+};
+
+export default LandingText;

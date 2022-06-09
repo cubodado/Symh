40ec2c2ce5aaa0d94c452601aa0636dec4193eae
cubodado <dalkang000@gmail.com>
2022-06-10 03:36:28 +0900

    Update: Step1 UI

--- src/StepPage/Step.js
+++ src/StepPage/Step.js
@@ -1,13 +1,20 @@
 import './Step.css';
+import { GrPrevious, GrNext } from 'react-icons/gr';
 
 const Step = () => {
   return (
-    <main>
-      <h2>Step 1</h2>
-      <h1>Write down your short letter.</h1>
-      <input></input>
-      <button>back</button>
-      <button>next</button>
+    <main className="step-div">
+      <h2 className="step-text">Step 1</h2>
+      <h1 className="step-info-text">Write down your short letter.</h1>
+      <input className="user-input"></input>
+      <div className="buttons">
+        <button>
+          <GrPrevious className="icons" />
+        </button>
+        <button>
+          <GrNext className="icons" />
+        </button>
+      </div>
     </main>
   );
 };

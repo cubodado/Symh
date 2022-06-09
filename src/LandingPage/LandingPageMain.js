09214fb8ffef49cce0fa1d2e333306d2b95b815c
cubodado <dalkang000@gmail.com>
2022-06-09 20:37:19 +0900

    Create: LoadingBar component

--- src/LandingPage/LandingPageMain.js
+++ src/LandingPage/LandingPageMain.js
@@ -1,14 +1,18 @@
 import './LandingPageMain.css';
 import PulsingHeart from './PulsingHeart';
+import LoadingBar from './LoadingBar';
 import { Fragment } from 'react';
 
 const LandingPageMain = () => {
   return (
     <Fragment>
-      <div className="landing-text">
-        <p id="landing-text">Send your moving heart</p>
-        <PulsingHeart />
-      </div>
+      <main className="landing-div">
+        <div className="landing-text">
+          <p id="landing-text">Send your moving heart</p>
+          <PulsingHeart />
+        </div>
+        <LoadingBar />
+      </main>
     </Fragment>
   );
 };

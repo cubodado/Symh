68fe336147255a6cacebf0f26c5eac5a9d8b474e
cubodado <dalkang000@gmail.com>
2022-06-10 00:46:48 +0900

    Create: Step component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import LandingPageMain from './LandingPage/LandingPageMain';
+import Step from './StepPage/Step';
+import { useState, useEffect } from 'react';
 
 function App() {
+  let [flag, setFlag] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFlag(true);
+    }, 4500);
+  });
+
   return (
     <div className="wrapper">
-      <LandingPageMain />
+      {flag === false ? <LandingPageMain /> : <Step />}
     </div>
   );
 }
---
+++ src/StepPage/Step.js
@@ -0,0 +1,15 @@
+import './Step.css';
+
+const Step = () => {
+  return (
+    <main>
+      <h2>Step 1</h2>
+      <h1>Write down your short letter.</h1>
+      <input></input>
+      <button>back</button>
+      <button>next</button>
+    </main>
+  );
+};
+
+export default Step;

ffaf54ec66d114205ade12007c4cd200f29b7cb5
cubodado <dalkang000@gmail.com>
2022-06-10 18:55:04 +0900

    Feat: get an step1 input's value by using useState

--- src/Steps/StepPages/Step1.js
+++ src/Steps/StepPages/Step1.js
@@ -2,15 +2,23 @@ import '../index.css';
 import './Step1.css';
 import NavigationButtons from '../StepUI/NavigationButtons';
 import StepInformation from '../StepUI/StepInformation';
+import { useState } from 'react';
 
 const Step1 = () => {
+  let [userInput, setUserInput] = useState('');
+
+  const changeEventHandler = () => {
+    const userInputText = document.querySelector('.user-input');
+    setUserInput(userInputText.value);
+  };
+
   return (
     <main className="step-div">
       <StepInformation
         stepNumber={1}
         stepInfoText={'Write down your short letter.'}
       />
-      <input className="user-input"></input>
+      <input onChange={changeEventHandler} className="user-input"></input>
       <NavigationButtons />
     </main>
   );

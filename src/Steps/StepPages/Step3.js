940bfccb240ffd611dc19cb90fa7a39458bdb04d
cubodado <dalkang000@gmail.com>
2022-06-12 00:48:23 +0900

    Feat: select heart anmiation

--- src/Steps/StepPages/Step3.js
+++ src/Steps/StepPages/Step3.js
@@ -3,18 +3,78 @@ import './Step3.css';
 import StepInformation from '../StepUI/StepInformation';
 import SelectBox from '../StepUI/SelectBox';
 import NavigationButtons from '../StepUI/NavigationButtons';
+import { useState } from 'react';
 
 const Step3 = () => {
   const alertMessage =
     'You can go to the next step if you select a heart animation.';
   const nextPage = '/step/4';
   const infoText = 'Select your heart animation.';
-  let heartAnimation = '';
+  const heartAnimations = [
+    {
+      id: 1,
+      number: 'animation1',
+      class: 'heart-animation',
+      content: '✌🏻',
+    },
+    {
+      id: 2,
+      number: 'animation2',
+      class: 'heart-animation',
+      content: '👋🏻',
+    },
+    {
+      id: 3,
+      number: 'animation3',
+      class: 'heart-animation',
+      content: '👏🏻',
+    },
+    {
+      id: 4,
+      number: 'animation4',
+      class: 'heart-animation',
+      content: '🙌🏻',
+    },
+  ];
+  const [heartAnimation, setHeartAnimation] = useState('');
+  const [check, setCheck] = useState(false);
+
+  const toggleHeartAnimation = (currentTarget) => {
+    currentTarget.classList.toggle('active');
+
+    if (heartAnimation === '') {
+      setHeartAnimation(currentTarget.id);
+      setCheck(true);
+    } else {
+      setHeartAnimation('');
+      setCheck(false);
+    }
+  };
+
+  const heartAnimationClickHandler = (event) => {
+    if (check && event.currentTarget.id !== heartAnimation) {
+      alert('You can select only one animation.');
+    } else {
+      toggleHeartAnimation(event.currentTarget);
+    }
+  };
 
   return (
     <main className="step-div">
       <StepInformation stepNumber={3} stepInfoText={infoText} />
-      <SelectBox />
+      <div className="select-box">
+        {heartAnimations.map((animation) => {
+          return (
+            <SelectBox
+              key={animation.id}
+              number={animation.number}
+              class={animation.class}
+              onClick={heartAnimationClickHandler}
+              content={animation.content}
+            />
+          );
+        })}
+      </div>
       <NavigationButtons
         stepNumber={3}
         nextCheck={heartAnimation}

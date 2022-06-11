d27f7c12ed4094030fa3e2ac6ec815deb2167a58
cubodado <dalkang000@gmail.com>
2022-06-11 20:52:27 +0900

    Feat: select heart shape

--- src/Steps/StepPages/Step2.js
+++ src/Steps/StepPages/Step2.js
@@ -2,14 +2,56 @@ import '../index.css';
 import './Step2.css';
 import StepInformation from '../StepUI/StepInformation';
 import NavigationButtons from '../StepUI/NavigationButtons';
+import { useState } from 'react';
 
 const Step2 = () => {
+  const [heartShape, setHeartShape] = useState('');
   const infoText = 'Select your heart shape.';
-  let heartShape = 1;
+
+  const heartShapeClickHandler = (event) => {
+    const currentTarget = event.currentTarget;
+    currentTarget.classList.toggle('active');
+
+    if (heartShape === '') {
+      setHeartShape(event.currentTarget.id);
+    } else {
+      setHeartShape('');
+    }
+  };
 
   return (
     <main className="step-div">
       <StepInformation stepNumber={2} stepInfoText={infoText} />
+      <div className="heart-shapes">
+        <div
+          id="shape1"
+          className="heart-shape"
+          onClick={heartShapeClickHandler}
+        >
+          <h2>💖</h2>
+        </div>
+        <div
+          id="shape2"
+          className="heart-shape"
+          onClick={heartShapeClickHandler}
+        >
+          <h2>💕</h2>
+        </div>
+        <div
+          id="shape3"
+          className="heart-shape"
+          onClick={heartShapeClickHandler}
+        >
+          <h2>💝</h2>
+        </div>
+        <div
+          id="shape4"
+          className="heart-shape"
+          onClick={heartShapeClickHandler}
+        >
+          <h2>💘</h2>
+        </div>
+      </div>
       <NavigationButtons stepNumber={2} nextCheck={heartShape} />
     </main>
   );

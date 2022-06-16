1e0f57b91c31a8770b5f8d5d1c2a3d1d55fe2c32
cubodado <dalkang000@gmail.com>
2022-06-17 02:42:59 +0900

    Refactor: branch stepText

--- src/components/layout/navigation/StepInformation/StepInformation.js
+++ src/components/layout/navigation/StepInformation/StepInformation.js
@@ -1,9 +1,13 @@
 import './StepInformation.css';
 
 const StepInformation = (props) => {
+  const stepText = props.stepNumber === 'Clear ✨' ? 'Steps' : 'Step';
+
   return (
     <div className="step-text-div">
-      <h2 className="step-text">Step {props.stepNumber}</h2>
+      <h2 className="step-text">
+        {stepText} {props.stepNumber}
+      </h2>
       <h1 className="step-info-text">{props.stepInfoText}</h1>
     </div>
   );

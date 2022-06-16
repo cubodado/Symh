5d924ee8cbe18365ad16fa184596f2b2dce7e785
cubodado <dalkang000@gmail.com>
2022-06-17 02:00:36 +0900

    Create: views/Steps/Step4

--- src/views/Steps/Step4/Step4.js
+++ src/views/Steps/Step4/Step4.js
@@ -1,27 +1,15 @@
-import '../index.css';
+import '../../../App.css';
 import './Step4.css';
-import {
-  NavigationButtons,
-  StepInformation,
-} from '../../../components/layout/navigation/index';
+import LandingPage from '../../../components/layout/navigation/LandingPage';
 import { useSelector } from 'react-redux';
 
 const Step4 = () => {
-  const alertMessage = 'Just wait for a second.';
-  const infoText = 'Alomost done.';
   const steps = useSelector((state) => state.steps);
   console.log(steps);
 
   return (
-    <div className="step-div">
-      <StepInformation stepNumber={4} stepInfoText={infoText} />
-      <NavigationButtons
-        stepNumber={4}
-        nextCheck={''}
-        alertMessage={alertMessage}
-        nextPage={''}
-        updateStore={null}
-      />
+    <div className="wrapper">
+      <LandingPage landingText={'Your heart skips a beat'} />
     </div>
   );
 };
